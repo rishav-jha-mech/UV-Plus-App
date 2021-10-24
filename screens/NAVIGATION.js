325046537eba97da975b5eabe54eb320caaf0bd2
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-24 15:35:52 +0530

    Stack nav added along with Tab Navigation

--- screens/NAVIGATION.js
+++ screens/NAVIGATION.js
@@ -1,68 +1,83 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 import Home from './Home';
 import Downloading from './Downloading';
 import Completed from './Completed';
 import Founder from './Founder';
+import Web from './Web.js'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faCheckCircle, faDownload, faHome, faUserNinja } from '@fortawesome/free-solid-svg-icons';
 
 
 const Tab = createBottomTabNavigator();
 
-const NAVIGATION = () => {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator screenOptions={{ headerShown: false }}> 
-                <Tab.Screen
-                    name="Home" 
-                    component={Home}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                          <FontAwesomeIcon icon={faHome} color={color} size={size} />
-                        ),
-                        tabBarActiveTintColor: '#ff156f',
-                        tabBarInactiveTintColor: '#999',
-                      }}
-                />
-                <Tab.Screen 
-                    name="Downloading" 
-                    component={Downloading} 
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                          <FontAwesomeIcon icon={faDownload} color={color} size={size} />
-                        ),
-                        tabBarActiveTintColor: '#ff156f',
-                        tabBarInactiveTintColor: '#999',
-                      }}
-                />
-                <Tab.Screen
-                    name="Completed"
-                    component={Completed}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                          <FontAwesomeIcon icon={faCheckCircle} color={color} size={size} />
-                        ),
-                        tabBarActiveTintColor: '#ff156f',
-                        tabBarInactiveTintColor: '#999',
-                      }}
-                    />
-                <Tab.Screen
-                    name="Founder"
-                    component={Founder}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                          <FontAwesomeIcon icon={faUserNinja} color={color} size={size} />
-                        ),
-                        tabBarActiveTintColor: '#ff156f',
-                        tabBarInactiveTintColor: '#999',
-                      }}
-                    />
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+const HomeTabNavigation = () => {
+  return (
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faHome} color={color} size={size} />
+            ),
+            tabBarActiveTintColor: '#ff156f',
+            tabBarInactiveTintColor: '#999',
+          }}
+        />
+        <Tab.Screen
+          name="Downloading"
+          component={Downloading}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faDownload} color={color} size={size} />
+            ),
+            tabBarActiveTintColor: '#ff156f',
+            tabBarInactiveTintColor: '#999',
+          }}
+        />
+        <Tab.Screen
+          name="Completed"
+          component={Completed}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faCheckCircle} color={color} size={size} />
+            ),
+            tabBarActiveTintColor: '#ff156f',
+            tabBarInactiveTintColor: '#999',
+          }}
+        />
+        <Tab.Screen
+          name="Founder"
+          component={Founder}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faUserNinja} color={color} size={size} />
+            ),
+            tabBarActiveTintColor: '#ff156f',
+            tabBarInactiveTintColor: '#999',
+          }}
+        />
+      </Tab.Navigator>
+  )
 }
+
+
+const NAVIGATION = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home Tab" component={HomeTabNavigation} />
+        <Stack.Screen name="Web Tab" component={Web} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
 
 export default NAVIGATION
