ac38d29ef456737964b35c892e6f9249f066efe9
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-25 22:04:39 +0530

    Audio for yt showing perfectly

--- screens/Components/AudioList.js
+++ screens/Components/AudioList.js
@@ -1,36 +1,55 @@
 import React,{ useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import bytesConverter from '../Scripts/bytesConverter'
 
-const AudioList = ( { info } ) => {
+const AudioList = ( { info, source } ) => {
 
-    const [stream, setStream] = useState(0)
     const [filesize, setFilesize] = useState(0)
-    const [url, setUrl] = useState()
     const [format, setFormat] = useState()
     const [ext, setExt] = useState()
 
-    useEffect(() => {
-        // For setting up formats and other stuffs before rendering
-        setStream(info.res)
-        setUrl(info.url)
-        setFilesize(info.filesize)
-        setExt(info.ext)
+    // Hooks for checking the platform
+    const [youtube, setYoutube] = useState(false)
+    const [facebook, setFacebook] = useState(false)
+    const [instagram, setInstagram] = useState(false)
+    
+    // Hooks for showing Audio only
+    const [audio, setAudio] = useState(false)
 
-        var regExp = /\(([^)]+)\)/;
-        var Localformat = regExp.exec(info.format);
-        setFormat(Localformat[1])
+    useEffect(() => {
+        // Checking the source of the audio file
+        if (source == 'youtube'){setYoutube(true);Youtube(info)}
+        if (source == 'facebook'){setFacebook(true);Facebook(info)}
+        if (source == 'Instagram'){setYoutube(true);Instagram(info)}
+        // For setting up formats and other stuffs before rendering
+        setExt(info.ext)
     }, [info])
 
-    return (
+
+    const Youtube = (info) => {
+        // Changing the format string
+        var regExp = /\(([^)]+)\)/;
+        var Localformat = regExp.exec(info.format);
+        setFormat(Localformat[1]);
+        // Show Audio files only
+        if (info.height == null) {
+            setAudio(true);
+            // Change file size from bytes to KB,MB,or GB
+            setFilesize(bytesConverter(info.filesize));
+        }
+    }
+
+    return (youtube && audio) ?(
         <View style={styles.Container}>
-            <Text style={[styles.TheText,styles.format]}> {format} </Text>
+            <Text style={[styles.TheText,styles.format]}> {format ? format : 'Not Present'} </Text>
             <Text style={styles.TheText}> {ext}</Text>
             <Text style={styles.TheText}> {filesize ? filesize : 'undefined'}</Text>
         </View>
-    )
+    ):(<></>)
 }
 
 export default AudioList
+
 
 const styles = StyleSheet.create({
     Container:{
