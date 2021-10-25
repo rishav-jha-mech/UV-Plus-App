a7ffe866233123d6ad5cb700c5c8fdcbfcc77b00
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-25 22:22:08 +0530

    Frontend ready Filter working for yt

--- screens/Components/VideoList.js
+++ screens/Components/VideoList.js
@@ -1,20 +1,72 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React,{ useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import bytesConverter from '../Scripts/bytesConverter'
 
-const VideoList = ( { info } ) => {
+const VideoList = ( { info, source } ) => {
+
+    const [filesize, setFilesize] = useState(0)
+    const [format, setFormat] = useState()
+    const [ext, setExt] = useState()
+
+    // Hooks for checking the platform
+    const [youtube, setYoutube] = useState(false)
+    const [facebook, setFacebook] = useState(false)
+    const [instagram, setInstagram] = useState(false)
+    
+    // Hooks for showing video only
+    const [audio, setAudio] = useState(false)
+
+    useEffect(() => {
+        // Checking the source of the video file
+        if (source == 'youtube'){setYoutube(true);Youtube(info)}
+        if (source == 'facebook'){setFacebook(true);Facebook(info)}
+        if (source == 'Instagram'){setYoutube(true);Instagram(info)}
+        // For setting up formats and other stuffs before rendering
+        setExt(info.ext)
+    }, [info])
 
 
-    return (
-        <Text style={styles.TheText}> info.ext </Text>
-    )
+    const Youtube = (info) => {
+        // Changing the format string
+        var regExp = /\(([^)]+)\)/;
+        var Localformat = regExp.exec(info.format);
+        setFormat(Localformat[1]);
+        // Show Video files only
+        if (info.height != null && info.ext == "mp4" && info.filesize != null) {
+            setAudio(true);
+            // Change file size from bytes to KB,MB,or GB
+            setFilesize(bytesConverter(info.filesize));
+        }
+    }
+
+    return (youtube && audio) ?(
+        <View style={styles.Container}>
+            <Text style={[styles.TheText,styles.format]}> {format ? format : 'Not Present'} </Text>
+            <Text style={styles.TheText}> {ext}</Text>
+            <Text style={styles.TheText}> {filesize ? filesize : 'undefined'}</Text>
+        </View>
+    ):(<></>)
 }
 
 export default VideoList
 
+
 const styles = StyleSheet.create({
+    Container:{
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
     TheText:{
-        fontSize:16,
-        backgroundColor:'white',
-        marginVertical:5
+        flex:1,
+        marginVertical: 3,
+        backgroundColor:"#ff56",
+        textAlign:'center',
+        paddingHorizontal:12,
+        paddingVertical:12,
+    },
+    format:{
+        textTransform:'capitalize',
+        textAlign:'left'
     }
 })
