24b973f43b3f8bfb09b96054dac7a413bab1d215
rishav-jha-mech <rishav.rj74@gmail.com>
2021-11-02 12:25:21 +0530

    Modal for file options working

--- screens/Components/FileList.js
+++ screens/Components/FileList.js
@@ -1,7 +1,7 @@
 import React,{ useState,useEffect } from 'react'
-import { Image, Pressable, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV, faInfo, faInfoCircle, faPen, faShareAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import bytesConverter from '../Scripts/bytesConverter'
 import TimeStampToDate from '../Scripts/TimeStampToDate';
 import formatFormatter from '../Scripts/formatFormatter'
@@ -17,6 +17,7 @@ const FileList = (data) => { // By default it is sorted by recent old order
     const [date, setDate] = useState(0)
     const [filename, setFilename] = useState("")
     const [ext, setExt] = useState("")
+    const [showmodal, setShowModal] = useState(false)
 
     useEffect(() =>{
         try{
@@ -44,9 +45,44 @@ const FileList = (data) => { // By default it is sorted by recent old order
                 <Text style={styles.Title} numberOfLines={2}>{filename}</Text>
                 <Text style={styles.SubTitle} numberOfLines={1}>{fileSize}&nbsp;&nbsp;|&nbsp;&nbsp;{ext}&nbsp;&nbsp;|&nbsp;&nbsp;{date}</Text>
             </View>
-            <TouchableOpacity style={styles.theButton}>
+            <TouchableOpacity style={styles.theButton} onPress={() => setShowModal(!showmodal)}>
                 <FontAwesomeIcon icon={faEllipsisV} size={18} />
             </TouchableOpacity>
+
+            <Modal visible={showmodal} transparent={true} animationType={"fade"}>
+                <Pressable style={styles.Modal} onPress={() => setShowModal(!showmodal)}>
+                    <View style={styles.Option}>
+                        <TouchableOpacity style={styles.OptionBtn}>
+                            <View style={styles.font}>
+                                <FontAwesomeIcon icon={faPen} size={16} color={'#555'} />
+                            </View>
+                            <Text style={styles.OptionTxt}> Rename</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.OptionBtn}>
+                            <View style={styles.font}>
+                                <FontAwesomeIcon icon={faTrashAlt} size={16} color={'#555'} />
+                            </View>
+                            <Text style={styles.OptionTxt}> Delete</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.OptionBtn}>
+                            <View style={styles.font}>
+                                <FontAwesomeIcon icon={faShareAlt} size={16} color={'#555'} />
+                            </View>
+                            <Text style={styles.OptionTxt}> Share</Text>
+                        </TouchableOpacity>
+
+                        
+                        <TouchableOpacity style={styles.OptionBtn}>
+                            <View style={styles.font}>
+                                <FontAwesomeIcon icon={faInfoCircle} size={16} color={'#555'} />
+                            </View>
+                            <Text style={styles.OptionTxt}> Info</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Pressable>
+            </Modal>
         </Pressable>
     )
 }
@@ -82,10 +118,45 @@ const styles = StyleSheet.create({
         fontSize:12
     },
     theButton:{
-        width:'8%',
-        // backgroundColor:'chartreuse',
+        width:'12%',
+        backgroundColor:'chartreuse',
         alignItems:'center',
         justifyContent:'center'
+    },
+    Modal:{
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    Option:{
+        backgroundColor:'#fff',
+        justifyContent:'space-between',
+        zIndex:10,
+        padding:12,
+        paddingHorizontal:16,
+        elevation:10,
+        borderRadius:4,
+        width:'55%',
+        height:'30%'
+    },
+    OptionBtn:{
+        marginVertical:5,
+        paddingHorizontal:8,
+        paddingVertical:8,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    OptionTxt:{
+        color:'#555',
+        fontWeight:'700',
+        fontSize:18,
+        letterSpacing: 1,
+        flex:6
+    },
+    font:{
+        flex:1,
     }
 })
 
