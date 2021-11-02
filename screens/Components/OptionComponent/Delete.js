bfd09339a521e9a6f0047de737f157f4c3ea9209
rishav-jha-mech <rishav.rj74@gmail.com>
2021-11-02 16:25:07 +0530

    Delete File FrontEnd ReadY

--- screens/Components/OptionComponent/Delete.js
+++ screens/Components/OptionComponent/Delete.js
@@ -1,14 +1,67 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, Pressable,View,TouchableOpacity } from 'react-native'
 
-const Delete = () => {
+const Delete = (data) => {
+
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Pressable style={styles.Container}>
+            <Text style={styles.Title}>Are you sure you want to delete this file ?</Text>
+            <Text style={styles.SubTitle} noOfLines={3}>File Name :  <Text style={styles.Details}>{data.name}</Text></Text>
+            <Text style={styles.SubTitle}>Ext :  <Text style={styles.Details}>.{data.ext}</Text></Text>
+            <Text style={styles.SubTitle}>File Size :  <Text style={styles.Details}>{data.size}</Text></Text>
+            <View style={styles.ButtonContainer}>
+                <TouchableOpacity style={styles.theButton}>
+                    <Text style={styles.theButtonText}>Delete</Text>
+                </TouchableOpacity>
+            </View>
+        </Pressable>
     )
 }
 
 export default Delete
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    Container:{
+        backgroundColor:'#fff',
+        elevation:10,
+        borderRadius:16,
+        width:'75%',
+        height:'43%',
+        zIndex:15,
+        paddingVertical:14,
+        paddingHorizontal:20
+    },
+    Title:{
+        fontSize:21,
+        color:'#000',
+        fontWeight:'700',
+        marginVertical: 12,
+        textTransform:'capitalize',
+        letterSpacing:1
+    },
+    SubTitle:{
+        marginVertical:10,
+        color:'#6f00ff',
+        fontWeight:'800'
+    },
+    Details:{
+        lineHeight:22,
+        color:'black',
+        fontWeight:'700'
+    },
+    ButtonContainer:{
+        alignItems:'flex-end'
+    },
+    theButton:{
+        backgroundColor:'crimson',
+        paddingHorizontal:16,
+        paddingVertical:9,
+        borderRadius: 5
+    },
+    theButtonText:{
+        color:'#fff',
+        fontSize:16,
+        fontWeight:'700',
+        letterSpacing: 0.8
+    }
+})
