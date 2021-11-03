eb9d9c3dd291f6d7e11cc87487d5db540d6c5241
rishav-jha-mech <rishav.rj74@gmail.com>
2021-11-04 02:16:38 +0530

    Frontend Ready

--- screens/Web.js
+++ screens/Web.js
@@ -1,10 +1,9 @@
 import React, { useState, useRef } from 'react'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, TextInput, TouchableOpacity, Modal, Text, Pressable } from 'react-native'
 import WebView from 'react-native-webview'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faArrowLeft, faArrowRight, faCopy, faEllipsisV, faHome, faRedo } from '@fortawesome/free-solid-svg-icons'
 import validator from 'validator'
-import { ScrollView } from 'react-native-gesture-handler'
 
 const HOMEPAGE = "https://www.google.com/"
 
@@ -16,6 +15,7 @@ const Web = ({ route }) => {
     const [URL, setURL] = useState(url)
     const [tempURL,setTempURL] = useState(URL)
     const [webkey,setWebKey] = useState(0) // Not a very good option, this does a force update on the webview component
+    const [showModal,setShowModal] =useState(false)
     const webViewRef = useRef();
 
     const Validate = (text) =>{ // If the input isnt a url then it will go to GoogleIt function will be called.
@@ -49,10 +49,23 @@ const Web = ({ route }) => {
                 <TouchableOpacity style={TopBar.Copy}>
                     <FontAwesomeIcon icon={faCopy} color={'#555'} size={19} />
                 </TouchableOpacity>
-                <TouchableOpacity style={TopBar.Opt}>
+                <TouchableOpacity style={TopBar.Opt} onPress={() => setShowModal(!showModal)}>
                     <FontAwesomeIcon icon={faEllipsisV} color={'#555'} size={20} />
                 </TouchableOpacity>
             </View>
+            
+            <Modal style={{flex:1}} visible={showModal} transparent={true} animationType={'fade'}>
+                <Pressable style={modalStyle.Container} onPress={() => setShowModal(!showModal)}>
+                    <Pressable style={modalStyle.Card}>
+                        <TouchableOpacity style={modalStyle.Button}><Text style={modalStyle.ButtonText}>Copy Link</Text></TouchableOpacity>
+                        <TouchableOpacity style={modalStyle.Button}><Text style={modalStyle.ButtonText}>Share Link</Text></TouchableOpacity>
+                        <TouchableOpacity style={modalStyle.Button}><Text style={modalStyle.ButtonText}>Reload</Text></TouchableOpacity>
+                        <TouchableOpacity style={modalStyle.Button}><Text style={modalStyle.ButtonText}>Clear Cache</Text></TouchableOpacity>
+                        <TouchableOpacity style={modalStyle.Button}><Text style={modalStyle.ButtonText}>Use Incognito Mode</Text></TouchableOpacity>
+                        <TouchableOpacity style={modalStyle.Button}><Text style={modalStyle.ButtonText}>Clear History (Forward/Back)</Text></TouchableOpacity>
+                    </Pressable>
+                </Pressable>
+            </Modal>
             <WebView
                 key={webkey}
                 source={{ uri: URL }}
@@ -80,6 +93,37 @@ const Web = ({ route }) => {
 
 export default Web
 
+const modalStyle = StyleSheet.create({
+    Container:{
+        flex:1,
+        backgroundColor:'rgba(0,0,0,0.3)',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    Card:{
+        backgroundColor:'#fff',
+        minHeight:'30%',
+        minWidth:'65%',
+        borderRadius:16,
+        elevation:8,
+        padding:16,
+    },
+    Button:{
+        marginVertical:6,
+        backgroundColor:'rgba(30, 143, 255,0.15)',
+        paddingVertical:10,
+        paddingHorizontal:16,
+        borderRadius:6,
+        borderWidth:0.6,
+        borderColor:'#ddd'
+    },
+    ButtonText:{
+        fontSize:16,
+        fontWeight:'700',
+        textAlign:'center',
+        color:'dodgerblue'
+    }
+})
 const TopBar = StyleSheet.create({
     Container: {
         paddingVertical: 10,
