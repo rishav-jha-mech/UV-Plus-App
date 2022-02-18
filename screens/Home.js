7cc16569344d6bdec912cf169a941108cba720a9
rishav-jha-mech <rishav.rj74@gmail.com>
2022-02-19 00:47:16 +0530

    Home Page design changes

--- screens/Home.js
+++ screens/Home.js
@@ -21,26 +21,29 @@ const Home = () => {
     
     return (
         <View style={styles.Container}>
-            <View style={styles.urlContainer}>
-                <TextInput
-                    style={styles.Input}
-                    placeholder="Enter URL"
-                    keyboardType="url"
-                    selectionColor="#333"
-                    defaultValue={url}
-                    onChangeText={(url) => setUrl(url)}
-                />
-                {/* Pressing this button will send the url to axios */}
-                <Pressable
-                    style={styles.Search}
-                    onPress={() => { PostReq(url); }}
-                >
-                    <FontAwesomeIcon
-                        icon={faSearch}
-                        size={24}
-                        color={"#ff156f"}
+            <View style={styles.Navbar}>
+                <Text style={styles.NavbarText}>Universal Downloader</Text>
+                <View style={styles.urlContainer}>
+                    <TextInput
+                        style={styles.Input}
+                        placeholder="Enter url"
+                        keyboardType="url"
+                        selectionColor="#333"
+                        defaultValue={url}
+                        onChangeText={(url) => setUrl(url)}
                     />
-                </Pressable>
+                    {/* Pressing this button will send the url to axios */}
+                    <Pressable
+                        style={styles.Search}
+                        onPress={() => { PostReq(url); }}
+                    >
+                        <FontAwesomeIcon
+                            icon={faSearch}
+                            size={24}
+                            color={"#ff156f"}
+                        />
+                    </Pressable>
+                </View>
             </View>
         </View>
     )
@@ -49,30 +52,46 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
+    Navbar:{
+        position:'relative',
+        paddingHorizontal: 16.0,
+        paddingVertical: 16.0,
+        height: 225,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: '#5959ff'
+    },
+    NavbarText:{
+        fontSize: 32.0,
+        fontWeight: '700',
+        color: '#fff'
+    },
     Container: {
         flex: 1,
-        backgroundColor: '#ddf'
+        backgroundColor: '#fcfcfc'
     },
     urlContainer: {
-        backgroundColor: 'dodgerblue',
+        position:'absolute',
+        bottom: -48,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        paddingVertical: 16,
+        paddingVertical: 20.0,
         paddingHorizontal: 12
     },
     Input: {
         backgroundColor: '#fff',
         flex: 1,
-        borderRadius: 24,
-        paddingLeft: 16,
+        borderRadius: 50.0,
+        padding: 16.0,
+        elevation: 12.0
     },
     Search: {
         position: 'absolute',
         alignItems: 'center',
         right: 16,
         padding: 10,
-        backgroundColor: '#fff',
+        backgroundColor: '#000',
         borderRadius: 25,
     },
     Message: {
