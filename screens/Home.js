87bb6e9fe398d130e31154f96ecbe596eadc61ac
rishav-jha-mech <rishav.rj74@gmail.com>
2022-02-19 10:33:23 +0530

    Initial Banner made

--- screens/Home.js
+++ screens/Home.js
@@ -1,97 +1,77 @@
 import React, { useState } from 'react'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { StyleSheet, Text, View, TextInput, Pressable, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput, StatusBar, ImageBackground, Pressable } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import ReadPermission from './Scripts/ReadPermission';
 
 const Home = () => {
     ReadPermission()
-    const navigation = useNavigation();
 
-    const [url, setUrl] = useState("")
-
-    const PostReq = (url) => {
-        console.log(url)
-
-        navigation.navigate('Result Tab', {
-            url: url,
-        });
-    }
-    
     return (
-        <View style={styles.Container}>
-            <View style={styles.Navbar}>
-                <Text style={styles.NavbarText}>Universal Downloader</Text>
-                <View style={styles.urlContainer}>
-                    <TextInput
-                        style={styles.Input}
-                        placeholder="Enter url"
-                        keyboardType="url"
-                        selectionColor="#333"
-                        defaultValue={url}
-                        onChangeText={(url) => setUrl(url)}
-                    />
-                    {/* Pressing this button will send the url to axios */}
-                    <Pressable
-                        style={styles.Search}
-                        onPress={() => { PostReq(url); }}
-                    >
-                        <FontAwesomeIcon
-                            icon={faSearch}
-                            size={24}
-                            color={"#ff156f"}
-                        />
-                    </Pressable>
+        <>
+        <View style={{backgroundColor: '#fcfcfc',flex:1}}>
+        <Banner />
+            <View style={styles.Container} forceInset={{ top: 'always' }}>
+                <View style={{ paddingHorizontal: 16.0 }}>
+                    <Text style={{ fontWeight: '700', fontSize: 16 }}>Your Recent Downloads</Text>
                 </View>
             </View>
         </View>
-    )
+        </>
+    );
 }
 
 export default Home
 
 const styles = StyleSheet.create({
-    Navbar:{
-        position:'relative',
-        paddingHorizontal: 16.0,
-        paddingVertical: 16.0,
+    Banner: {
         height: 225,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor: '#5959ff'
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#66f',
+        marginBottom:50
     },
-    NavbarText:{
+    BannerImageBG: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: 'center',
+        width: "100%",
+    },
+    NavbarText: {
         fontSize: 32.0,
         fontWeight: '700',
         color: '#fff'
     },
     Container: {
         flex: 1,
-        backgroundColor: '#fcfcfc'
+        backgroundColor: '#fcfcfc',
     },
     urlContainer: {
-        position:'absolute',
+        position: 'absolute',
         bottom: -48,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
         paddingVertical: 20.0,
-        paddingHorizontal: 12
+        paddingHorizontal: 12,
     },
     Input: {
         backgroundColor: '#fff',
         flex: 1,
         borderRadius: 50.0,
         padding: 16.0,
-        elevation: 12.0
+        elevation: 12.0,
+        zIndex: 100
     },
     Search: {
         position: 'absolute',
+        elevation: 13.0,
         alignItems: 'center',
         right: 16,
-        padding: 10,
-        backgroundColor: '#000',
+        padding: 14.0,
+        backgroundColor: '#66f',
         borderRadius: 25,
     },
     Message: {
@@ -103,3 +83,47 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
     },
 })
+const Banner = () => {
+    const navigation = useNavigation();
+    const BANNER = { uri: "https://raw.githubusercontent.com/byprogrammers/LCRN10-cryptocurrency-app-starter/master/assets/images/banner.png" };
+    
+    const [url, setUrl] = useState("")
+
+    const PostReq = (url) => {
+        console.log(url)
+        navigation.navigate('Result Tab', {
+            url: url,
+        });
+    }
+    return (<>
+        <StatusBar
+            animated={true}
+            backgroundColor="#6E55F7"
+        />
+        <View style={styles.Banner}>
+            <ImageBackground source={BANNER} style={styles.BannerImageBG} resizeMode={"cover"} >
+                <Text style={styles.NavbarText}>Universal Downloader</Text>
+            </ImageBackground>
+            <View style={styles.urlContainer}>
+                <TextInput
+                    style={styles.Input}
+                    placeholder="Enter url"
+                    keyboardType="url"
+                    selectionColor="#333"
+                    defaultValue={url}
+                    onChangeText={(url) => setUrl(url)}
+                />
+                <Pressable
+                    style={styles.Search}
+                    onPress={() => { PostReq(url); }}
+                >
+                    <FontAwesomeIcon
+                        icon={faSearch}
+                        size={24}
+                        color={"#fff"}
+                    />
+                </Pressable>
+            </View>
+        </View>
+    </>);
+}
